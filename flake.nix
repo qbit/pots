41ea7c6e92587d1e80af8cd81c20c19e080bcdd8
Aaron Bieber <aaron@bolddaemon.com>
2025-10-10 09:11:27 -0600

    fmt and version bump to v1.0.0

--- flake.nix
+++ flake.nix
@@ -3,40 +3,60 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in {
+    in
+    {
       overlay = final: prev: {
         pots = self.packages.${prev.system}.pots;
       };
       nixosModule = import ./module.nix;
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           pots = pkgs.buildGoModule {
             pname = "pots";
-            version = "v0.0.2";
+            version = "v1.0.0";
             src = ./.;
             vendorHash = null;
           };
-        });
+        }
+      );
 
       defaultPackage = forAllSystems (system: self.packages.${system}.pots);
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           default = pkgs.mkShell {
             shellHook = ''
               PS1='\u@\h:\@; '
               echo "Go `${pkgs.go}/bin/go version`"
             '';
-            nativeBuildInputs = with pkgs; [ git go gopls go-tools jo jq ];
+            nativeBuildInputs = with pkgs; [
+              git
+              go
+              gopls
+              go-tools
+              jo
+              jq
+            ];
           };
-        });
+        }
+      );
     };
 }
-
